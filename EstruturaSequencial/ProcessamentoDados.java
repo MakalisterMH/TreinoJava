48cfd3ca05473816f66b5ef8adb560e4d2e44ae0
Makalister <Makalisterhonorato@gmail.com>
2025-02-01 15:18:58 -0300

    Processamento de dados basicos

---
+++ EstruturaSequencial/ProcessamentoDados.java
@@ -0,0 +1,18 @@
+package EstruturaSequencial;
+
+public class ProcessamentoDados {
+
+    public static void main(String[] args) {
+
+        int x, y;
+
+        x = 5;
+
+        y = 2 * x;
+
+        System.out.println(y);
+        System.out.println(x);
+
+    }
+
+}

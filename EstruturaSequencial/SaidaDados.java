76bb764bee2b88aa60325cc18ecb62967fa43bda
Makalister <Makalisterhonorato@gmail.com>
2025-02-01 15:15:39 -0300

    Saida de dados em Java

---
+++ EstruturaSequencial/SaidaDados.java
@@ -0,0 +1,17 @@
+package EstruturaSequencial;
+
+public class SaidaDados {
+
+    public static void main(String[] args) {
+
+        System.out.print("Olá ");
+        System.out.println("mundo!");
+
+
+        System.out.println("Makalister Marques Honorato");
+        System.out.println("makalisterhonorato@gmail.com");
+        System.out.println("(62)98153-0099)");
+
+    }
+
+}

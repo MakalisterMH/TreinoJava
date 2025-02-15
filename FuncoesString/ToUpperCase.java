86ab427a4c7438fca10f5673c379366d859984f4
Makalister <Makalisterhonorato@gmail.com>
2025-02-14 23:15:47 -0300

    Função To Upper Case em java

---
+++ FuncoesString/ToUpperCase.java
@@ -0,0 +1,11 @@
+package FuncoesString;
+
+public class ToUpperCase {
+    public static void main(String[] args) {
+
+        String nome = "MaKaLiStEr MaRqUeS hOnOrAtO";
+
+        System.out.println(nome.toUpperCase());
+
+    }
+}

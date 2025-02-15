4d8e6234c07d32d6a5d406260ef432a6ee69fb05
Makalister <Makalisterhonorato@gmail.com>
2025-02-14 23:22:14 -0300

    Função Replace para fazer substituições na String

---
+++ FuncoesString/Replace.java
@@ -0,0 +1,13 @@
+package FuncoesString;
+
+public class Replace {
+    public static void main(String[] args) {
+
+        String nome = "Makalister";
+
+        System.out.println(nome.replace("k", "c")); // Trocando o "K" pelo "C"
+
+        System.out.println(nome.replace("Makalister", "Magaiver")); // Serve para a string completa ...
+
+    }
+}

123fd08927d6675b24fdba9383c683fe4411d192
Makalister <Makalisterhonorato@gmail.com>
2025-02-15 18:50:12 -0300

    Funcão Math.abs para retornar o valor absoluto de um numero (valor sem sinal)... sempre um numero positivo

---
+++ Math/MathAbs.java
@@ -0,0 +1,12 @@
+package Math;
+
+public class MathAbs {
+    public static void main(String[] args) {
+
+        double numero = -3.1;
+
+        double resultado = Math.abs(numero);  // Retorna o valor absoluto ou seja sem sinal, concluimos então q sempre vai ser positivo ...
+
+        System.out.println("Resultado: " + resultado);  
+    }
+}

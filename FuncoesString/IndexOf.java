3c0c4307dad6c9196386df09489f0030b8acddd7
Makalister <Makalisterhonorato@gmail.com>
2025-02-14 23:19:11 -0300

    Index Of para pegar posição da primeira ocorrencia

---
+++ FuncoesString/IndexOf.java
@@ -0,0 +1,12 @@
+package FuncoesString;
+
+public class IndexOf {
+
+    public static void main(String[] args) {
+
+        String nome = "Makalister";
+
+        System.out.println(nome.indexOf("a")); // Mostra a posição da ultima ocorrencia "a" ...
+
+    }
+}

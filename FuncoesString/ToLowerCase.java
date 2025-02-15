7909f2da9b464c748d1959fcafa3f6c1cf8381de
Makalister <Makalisterhonorato@gmail.com>
2025-02-14 23:16:59 -0300

    Função To Lower Case em java

---
+++ FuncoesString/ToLowerCase.java
@@ -0,0 +1,11 @@
+package FuncoesString;
+
+public class ToLowerCase {
+
+    public static void main(String[] args) {
+
+        String nome = "MaKaLiStEr MaRqUeS hOnOrAtO";
+
+        System.out.println(nome.toLowerCase());
+    }
+}

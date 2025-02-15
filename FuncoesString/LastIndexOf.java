ac03c5d5478596203f104804841aee3ad0cfdc56
Makalister <Makalisterhonorato@gmail.com>
2025-02-14 23:19:48 -0300

    Index Of para pegar posição da ultima ocorrencia

---
+++ FuncoesString/LastIndexOf.java
@@ -0,0 +1,12 @@
+package FuncoesString;
+
+public class LastIndexOf {
+
+    public static void main(String[] args) {
+
+        String nome = "Makalister";
+
+        System.out.println(nome.lastIndexOf("a")); // Mostra a posição da ultima ocorrencia "a" ...
+
+    }
+}

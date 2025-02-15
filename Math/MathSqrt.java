03cf32672c480f4aa6002549613a7474c7061f71
Makalister <Makalisterhonorato@gmail.com>
2025-02-15 18:44:30 -0300

    Funcão Math.sqrt para pegar o valor da raiz quadrada

---
+++ Math/MathSqrt.java
@@ -0,0 +1,15 @@
+package Math;
+
+public class MathSqrt {
+
+    public static void main(String[] args) {
+
+        int numero = 25;
+
+        int resultado = (int) Math.sqrt(numero);
+
+        System.out.println("Resultado: " + resultado);
+
+    }
+
+}

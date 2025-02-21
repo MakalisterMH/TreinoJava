93a8a3184c6f710b011b9693ff954ec633f6ca11
Makalister <Makalisterhonorato@gmail.com>
2025-02-20 21:05:37 -0300

    MathPow para calcular potencias em java

---
+++ Math/MathPow.java
@@ -0,0 +1,14 @@
+package Math;
+
+public class MathPow {
+
+    public static void main(String[] args) {
+
+        int numero = 2;
+        int elevado = 4;
+
+        System.out.println("O Numero " + numero + " elevado a " + elevado + " é igual á : " + Math.pow(numero, elevado));
+
+    }
+
+}
